4a7e2f49399a233a9eb837217f36f1f111793a20
Justin Reeves <reeves122@gmail.com>
2022-05-02 15:38:43 -0500

    update module ref format

--- examples/with_load_balancer/main.tf
+++ examples/with_load_balancer/main.tf
@@ -11,14 +11,14 @@ data "aws_route53_zone" "this" {
 }
 
 module "vpc" {
-  source                   = "git::git@github.com:champ-oss/terraform-aws-vpc.git?ref=v1.0.1-afc8890"
+  source                   = "github.com/champ-oss/terraform-aws-vpc.git?ref=v1.0.1-afc8890"
   git                      = local.git
   availability_zones_count = 2
   retention_in_days        = 1
 }
 
 module "acm" {
-  source            = "git::git@github.com:champ-oss/terraform-aws-acm.git?ref=v1.0.1-1cb7679"
+  source            = "github.com/champ-oss/terraform-aws-acm.git?ref=v1.0.1-1cb7679"
   git               = local.git
   domain_name       = data.aws_route53_zone.this.name
   zone_id           = data.aws_route53_zone.this.zone_id
@@ -26,7 +26,7 @@ module "acm" {
 }
 
 module "alb" {
-  source          = "git::git@github.com:champ-oss/terraform-aws-alb.git?ref=dfa6af4a7490ad56c85548e2364be4ea3e72b3d4"
+  source          = "github.com/champ-oss/terraform-aws-alb.git?ref=dfa6af4a7490ad56c85548e2364be4ea3e72b3d4"
   git             = local.git
   certificate_arn = module.acm.arn
   subnet_ids      = module.vpc.public_subnets_ids
