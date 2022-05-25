2a54a80fd659d856ce2ac7eff4390d0a9c2cdcbb
Justin Reeves <reeves122@gmail.com>
2022-05-25 16:31:39 -0500

    add cors options to aws_lambda_function_url

--- function-url.tf
+++ function-url.tf
@@ -2,4 +2,13 @@ resource "aws_lambda_function_url" "this" {
   count              = var.enable_function_url ? 1 : 0
   function_name      = aws_lambda_function.this.function_name
   authorization_type = var.function_url_authorization_type
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
 }
