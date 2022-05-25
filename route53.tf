ad187e2d037475bfeef7d35063325676990eb324
Justin Reeves <reeves122@gmail.com>
2022-05-25 10:48:44 -0500

    create separate route53 for function url

--- route53.tf
+++ route53.tf
@@ -1,12 +1,20 @@
 resource "aws_route53_record" "this" {
-  count   = var.enable_route53 ? 1 : 0
+  count   = var.enable_route53 && var.enable_load_balancer ? 1 : 0
   name    = var.dns_name
   type    = "A"
   zone_id = var.zone_id
 
   alias {
-    name                   = var.enable_function_url ? aws_lambda_function_url.this[0].function_url : var.lb_dns_name
-    zone_id                = var.enable_function_url ? var.zone_id : var.lb_zone_id
+    name                   = var.lb_dns_name
+    zone_id                = var.lb_zone_id
     evaluate_target_health = false
   }
+}
+
+resource "aws_route53_record" "function_url" {
+  count   = var.enable_route53 && var.enable_function_url ? 1 : 0
+  name    = var.dns_name
+  type    = "CNAME"
+  zone_id = var.zone_id
+  records = [aws_lambda_function_url.this[0].function_url]
 }
