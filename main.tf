1a26c2e8f99f649946df2d2143b2eb47c8ffb246
Justin Reeves <reeves122@gmail.com>
2022-05-31 11:29:35 -0500

    update null_resource wait_for_ecr REGISTRY_ID

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ locals {
 
 resource "null_resource" "wait_for_ecr" {
   triggers = {
-    ecr_account = var.ecr_account
+    ecr_account = local.ecr_account
     ecr_name    = var.ecr_name
     ecr_tag     = var.ecr_tag
   }
@@ -26,7 +26,7 @@ resource "null_resource" "wait_for_ecr" {
       AWS_REGION  = data.aws_region.this.name
       ECR_REPO    = var.ecr_name
       IMAGE_TAG   = var.ecr_tag
-      REGISTRY_ID = var.ecr_account
+      REGISTRY_ID = local.ecr_account
     }
   }
 }
