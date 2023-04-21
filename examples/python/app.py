a189a97630d335d25acfa05419795da2a4f6541e
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-04-21 16:24:03 -0500

    testing lambda with kms

--- examples/python/app.py
+++ examples/python/app.py
@@ -5,16 +5,14 @@ import boto3
 
 from base64 import b64decode
 
-test = boto3.client('kms').decrypt(
-    CiphertextBlob=b64decode(ENCRYPTED),
-    EncryptionContext={'terraform-aws-lambda-docker-hub': os.environ['FOO2']}
-)['Plaintext'].decode('utf-8')
+ENCRYPTED_FOO2 = os.environ['FOO2']
+FOO2 = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_FOO2))['Plaintext'].decode('utf-8')
 
 def handler(event, context):
     print("event: ", event)
     print("context: ", context)
     print("successful")
-    logger.info('testing output variable', test)
+    logger.info('testing output variable', FOO2)
     return {
         'statusCode': 200,
         'body': json.dumps('successful')
