41e87f438f61c45ef87df75b1cdac6101cabf456
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-04-21 15:58:54 -0500

    testing lambda with kms

--- examples/python/app.py
+++ examples/python/app.py
@@ -1,12 +1,13 @@
 import json
 import os
+import logging
 
 def handler(event, context):
     print("event: ", event)
     print("context: ", context)
     print("successful")
     test = os.environ['FOO2']
-    print(test)
+    logger.info('testing output variable', test)
     return {
         'statusCode': 200,
         'body': json.dumps('successful')
