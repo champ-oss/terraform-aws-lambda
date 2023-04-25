cc02132543ff13cffbf90a297e5d0769c7e200a5
Justin Reeves <reeves122@gmail.com>
2023-04-25 15:46:44 -0500

    update hash module path

--- examples/ecr_docker_image/main.tf
+++ examples/ecr_docker_image/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 module "hash" {
   source   = "github.com/champ-oss/terraform-git-hash.git?ref=v1.0.12-fc3bb87"
-  path     = "${path.module}/.."
+  path     = "${path.module}/../.."
   fallback = ""
 }
 
--- examples/with_load_balancer/main.tf
+++ examples/with_load_balancer/main.tf
@@ -64,7 +64,7 @@ module "alb" {
 
 module "hash" {
   source   = "github.com/champ-oss/terraform-git-hash.git?ref=v1.0.12-fc3bb87"
-  path     = "${path.module}/.."
+  path     = "${path.module}/../.."
   fallback = ""
 }
 
