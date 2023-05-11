729fd08d08940bf1046b032d15512d88e9318078
Justin Reeves <reeves122@gmail.com>
2023-05-11 09:55:38 -0500

    add checks for create_api_gateway_v1_resource

--- apigateway.tf
+++ apigateway.tf
@@ -16,7 +16,7 @@ resource "aws_api_gateway_method" "this" {
 resource "aws_api_gateway_integration" "this" {
   count                   = var.enable_api_gateway_v1 ? 1 : 0
   rest_api_id             = var.api_gateway_v1_rest_api_id
-  resource_id             = aws_api_gateway_resource.this[0].id
+  resource_id             = var.create_api_gateway_v1_resource ? aws_api_gateway_resource.this[0].id : var.api_gateway_v1_resource_id
   http_method             = aws_api_gateway_method.this[0].http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -28,7 +28,7 @@ resource "aws_api_gateway_deployment" "this" {
   rest_api_id = var.api_gateway_v1_rest_api_id
   triggers = {
     redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.this[0].id,
+      var.create_api_gateway_v1_resource ? aws_api_gateway_resource.this[0].id : var.api_gateway_v1_resource_id,
       aws_api_gateway_method.this[0].id,
       aws_api_gateway_integration.this[0].id
     ]))
