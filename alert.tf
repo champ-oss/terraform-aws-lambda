c9ac2729bf88b730692e0734d10d694f4a9553d9
Justin Reeves <reeves122@gmail.com>
2023-06-30 10:14:05 -0500

    add cloudwatch log module

---
+++ alert.tf
@@ -0,0 +1,10 @@
+module "alert" {
+  count          = var.enable_logging_alerts ? 1 : 0
+  source         = "github.com/champ-oss/terraform-aws-alert.git?ref=v1.0.133-a275efb"
+  git            = var.git
+  log_group_name = aws_cloudwatch_log_group.this.name
+  name           = var.name
+  filter_pattern = var.alert_filter_pattern
+  slack_url      = var.alert_slack_url
+  region         = var.alert_region
+}
--- variables.tf
+++ variables.tf
@@ -277,3 +277,27 @@ variable "custom_iam_policy_arn" {
   type        = string
   default     = null
 }
+
+variable "alert_slack_url" {
+  description = "Slack URL to use for alerts"
+  type        = string
+  default     = "https://hooks.slack.com/services/abc123"
+}
+
+variable "alert_region" {
+  description = "AWS region for alerting"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "alert_filter_pattern" {
+  description = "https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/FilterAndPatternSyntax.html#extract-log-event-values"
+  type        = string
+  default     = "ERROR"
+}
+
+variable "enable_logging_alerts" {
+  description = "Enable alerts for log messages"
+  type        = bool
+  default     = false
+}
