68ef9f96505e02b13c14aecb6fb96a97411d60ab
Justin Reeves <reeves122@gmail.com>
2023-06-30 10:57:17 -0500

    add alert to the name

--- alert.tf
+++ alert.tf
@@ -3,7 +3,7 @@ module "alert" {
   source         = "github.com/champ-oss/terraform-aws-alert.git?ref=v1.0.133-a275efb"
   git            = var.git
   log_group_name = aws_cloudwatch_log_group.this.name
-  name           = var.name
+  name           = "${var.name}-alert"
   filter_pattern = var.alert_filter_pattern
   slack_url      = var.alert_slack_url
   region         = var.alert_region
