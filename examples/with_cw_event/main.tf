b184cefa2f90a1774e62336c9a2364cf369cfe92
Justin Reeves <reeves122@gmail.com>
2024-03-11 16:11:25 -0500

    Add duplicate lambda to test for possible naming collisions

--- examples/with_cw_event/main.tf
+++ examples/with_cw_event/main.tf
@@ -4,7 +4,24 @@ data "archive_file" "this" {
   output_path = "package.zip"
 }
 
-module "this" {
+module "this1" {
+  source                         = "../../"
+  git                            = "terraform-aws-lambda"
+  name                           = "cloudwatch"
+  filename                       = data.archive_file.this.output_path
+  source_code_hash               = data.archive_file.this.output_base64sha256
+  handler                        = "app.handler"
+  runtime                        = "python3.9"
+  enable_cw_event                = true
+  schedule_expression            = "rate(1 minute)"
+  reserved_concurrent_executions = 1
+  environment = {
+    "FOO" = "BAR"
+  }
+}
+
+# Test for possible naming collisions
+module "this2" {
   source                         = "../../"
   git                            = "terraform-aws-lambda"
   name                           = "cloudwatch"
@@ -22,10 +39,10 @@ module "this" {
 
 output "arn" {
   description = "Lambda ARN"
-  value       = module.this.arn
+  value       = module.this1.arn
 }
 
 output "cloudwatch_log_group" {
   description = "alarm name output"
-  value       = module.this.cloudwatch_log_group
+  value       = module.this1.cloudwatch_log_group
 }
