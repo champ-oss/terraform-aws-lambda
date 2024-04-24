57b286cca731e51dbe0004b799556328afa1b4e0
Justin Reeves <reeves122@gmail.com>
2024-04-24 10:05:20 -0500

    Use trimmed name if enable_iam_role_name_prefix is disabled

--- examples/disable_iam_name_prefix/main.tf
+++ examples/disable_iam_name_prefix/main.tf
@@ -34,11 +34,6 @@ module "this" {
   enable_iam_role_name_prefix    = false # disable randomly generated name
 }
 
-output "function_name" {
-  description = "Lambda ARN"
-  value       = module.this.function_name
-}
-
 output "role_arn" {
   description = "Lambda ARN"
   value       = module.this.role_arn
--- examples/disable_iam_name_prefix/test.sh
+++ examples/disable_iam_name_prefix/test.sh
@@ -1,11 +1,5 @@
 set -e
 
-if [[ "$FUNCTION_NAME" != "terraform-aws-lambda-disable-iam-name-prefix" ]]; then
-    echo "function name is not correct"
-    exit 1
-fi
-
-
 if [[ "$ROLE_ARN" != "arn:aws:iam::912455136424:role/terraform-aws-lambda-disable-iam-name-prefix" ]]; then
     echo "role arn is not correct"
     exit 1
