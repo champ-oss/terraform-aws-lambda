acbb90601d9b86c2a84cf03ba2147a5729e93d5c
Justin Reeves <reeves122@gmail.com>
2024-04-24 09:58:41 -0500

    add echos to if statements

--- examples/disable_iam_name_prefix/test.sh
+++ examples/disable_iam_name_prefix/test.sh
@@ -1,5 +1,12 @@
 set -e
 
-[[ "$FUNCTION_NAME" = "terraform-aws-lambda-disable-iam-name-prefix" ]] || exit 1
+if [[ "$FUNCTION_NAME" != "terraform-aws-lambda-disable-iam-name-prefix" ]]; then
+    echo "function name is not correct"
+    exit 1
+fi
 
-[[ "$ROLE_ARN" = "arn:aws:iam::912455136424:role/terraform-aws-lambda-disable-iam-name-prefix" ]] || exit 1
+
+if [[ "$ROLE_ARN" != "arn:aws:iam::912455136424:role/terraform-aws-lambda-disable-iam-name-prefix" ]]; then
+    echo "role arn is not correct"
+    exit 1
+fi
