0fbeb3743f3e5297d9e4b92ef3843c520abe1847
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-05 13:37:09 -0500

    resolve index issue

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_group" "this" {
   count             = var.enabled ? 1 : 0
-  name              = "/aws/lambda/${var.git}-${var.name}-${random_id.this.hex}"
+  name              = "/aws/lambda/${var.git}-${var.name}-${random_id.this[0].hex}"
   retention_in_days = var.retention_in_days
   tags              = merge(local.tags, var.tags)
 
