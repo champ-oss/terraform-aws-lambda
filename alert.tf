846a99d2f9dda59678d245271090e50ab456a1af
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-08 16:36:08 -0500

    testing toggle of lambda

--- alert.tf
+++ alert.tf
@@ -1,7 +1,7 @@
 module "alert" {
   source         = "github.com/champ-oss/terraform-aws-alert.git?ref=d345d897de350e12a7545cb4061ab1901d31fbc2"
   git            = var.git
-  log_group_name = aws_cloudwatch_log_group.this.name
+  log_group_name = try(aws_cloudwatch_log_group.this[0].name, "")
   name           = "${var.name}-alert"
   filter_pattern = var.alert_filter_pattern
   slack_url      = var.alert_slack_url
