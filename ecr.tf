1a4c25bbcf8abefb5e3be33baf3aaf4ae0fdcadd
neena-champ <neena.deshpande@champtitles.com>
2026-02-26 13:47:55 -0600

    Update ecr-cache tag for image scanning update CLOUD-4025

--- ecr.tf
+++ ecr.tf
@@ -1,6 +1,6 @@
 module "ecr_cache" {
   count            = var.sync_image ? 1 : 0
-  source           = "github.com/champ-oss/terraform-aws-ecr-cache.git?ref=v1.0.18-4e903ea"
+  source           = "github.com/champ-oss/terraform-aws-ecr-cache.git?ref=v1.0.19-e55510e"
   name             = local.ecr_name
   sync_source_repo = var.sync_source_repo
   sync_source_tag  = var.ecr_tag
